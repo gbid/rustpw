3beb9fea016c1c15a32e6d83003980aa02385b73
Gunther Bidlingmaier <g.bidlingmaier@gmail.com>
2023-06-08 19:46:42 +0200

    1. Fix parsing error for when indentation level decreases 2. Provide user result of search

--- src/main.rs
+++ src/main.rs
@@ -1,11 +1,40 @@
 mod parse;
-use parse::{parse_entries, Entry};
-use std::{fs, io};
 
-fn main() -> io::Result<()> {
-    let content = fs::read_to_string("test_file")?;
-    let res: Option<Vec<Entry>> = parse_entries(&mut content.lines(), 0);
-    println!("{:#?}", res);
+use std::env;
+use std::fs;
+use parse::*;
 
-    Ok(())
+fn main() {
+    // Get the command line arguments
+    let args: Vec<String> = env::args().collect();
+
+    // Check if a pattern was provided
+    if args.len() < 3 {
+        println!("Please provide a pattern to search for and a file to serach in as arguments.");
+        return;
+    }
+
+    // The second argument is the pattern to search for
+    let pattern = &args[1];
+    let filename = &args[2];
+
+    // Read and parse the file
+    //let content = fs::read_to_string("/home/gbid/test_pws")
+    let content = fs::read_to_string(filename)
+        .expect(format!("Specified file {} does not exist", filename).as_str());
+
+    let entries = parse_entries(&mut content.lines().peekable(), 0).expect("Content was ill formatted");
+
+    // Search for the pattern
+    let found_entries = search_pattern(pattern, &entries);
+
+    // If we found some entries, present them to the user
+    if !found_entries.is_empty() {
+        for entry in found_entries {
+            present_entry(entry.clone(), "");
+        }
+    } else {
+        println!("No entries found for the pattern '{}'", pattern);
+    }
 }
+
--- src/parse.rs
+++ src/parse.rs
@@ -28,21 +28,112 @@ impl Entry {
     }
 }
 
+use clipboard::ClipboardProvider;
+use clipboard::ClipboardContext;
+use std::thread::sleep;
+use std::time::Duration;
+use std::io;
+fn copy_value(val: String, path: &str) {
+    let mut ctx: ClipboardContext = ClipboardProvider::new().unwrap();
+    ctx.set_contents(val.to_owned()).unwrap();
+    let seconds_to_clipboard_clean = 10;
+    println!("Copied value to clipboard for key path:\n{}", path);
+    println!("Value remains in clipboard for {} seconds", seconds_to_clipboard_clean);
+    
+    // wait for 10 seconds
+    sleep(Duration::from_secs(seconds_to_clipboard_clean));
+
+    // clear the clipboard
+    ctx.set_contents("".to_string()).unwrap();
+    println!("Cleared clipboard");
+}
+
+fn present_subentries(entries: &[Entry], path: &str) {
+    let mut input = String::new();
+    for (i, entry) in entries.iter().enumerate() {
+        println!("{}: {}", i + 1, entry.key);
+    }
+    io::stdin().read_line(&mut input).unwrap();
+    let choice = input.trim().parse::<usize>().unwrap();
+    if choice > 0 && choice <= entries.len() {
+        present_entry(entries[choice - 1].clone(), path);
+    }
+}
+
+pub fn present_entry(entry: Entry, path: &str) {
+    let next_path = if path.is_empty() {
+        entry.key.clone()
+    } else {
+        format!("{}->{}", path, entry.key)
+    };
+    match entry.val {
+        EntryVal::Value(val) => {
+            copy_value(val, &next_path);
+        },
+        EntryVal::SubEntries(entries) => {
+            present_subentries(&entries, &next_path);
+        },
+    }
+}
+
+
+//fn copy_value(val: String) {
+//    let mut ctx: ClipboardContext = ClipboardProvider::new().unwrap();
+//    ctx.set_contents(val.to_owned()).unwrap();
+//    
+//    // wait for 10 seconds
+//    sleep(Duration::from_secs(10));
+//
+//    // clear the clipboard
+//    ctx.set_contents("".to_string()).unwrap();
+//}
+//
+//fn present_subentries(entries: &[Entry]) {
+//    let mut input = String::new();
+//    for (i, entry) in entries.iter().enumerate() {
+//        println!("{}: {}", i + 1, entry.key);
+//    }
+//    io::stdin().read_line(&mut input).unwrap();
+//    let choice = input.trim().parse::<usize>().unwrap();
+//    if choice > 0 && choice <= entries.len() {
+//        present_entry(entries[choice - 1].clone());
+//    }
+//}
+//
+//pub fn present_entry(entry: Entry) {
+//    match entry.val {
+//        EntryVal::Value(val) => {
+//            copy_value(val);
+//        },
+//        EntryVal::SubEntries(entries) => {
+//            present_subentries(&entries);
+//        },
+//    }
+//}
+
+
 pub fn search_pattern<'a>(pattern: &str, entries: &'a [Entry]) -> Vec<&'a Entry> {
     entries
         .iter()
         .flat_map(|entry| entry.search_pattern(pattern))
         .collect()
 }
-pub fn parse_entries(lines: &mut std::str::Lines, indent_lvl: usize) -> Option<Vec<Entry>> {
+
+use std::iter::Peekable;
+
+pub fn parse_entries(lines: &mut Peekable<std::str::Lines>, indent_lvl: usize) -> Option<Vec<Entry>> {
     let mut entries = Vec::new();
-    while let Some(line) = lines.next() {
+    while let Some(line) = lines.peek().cloned() { // clone the line in peek
         if line.is_empty() {
+            lines.next(); // consume the line
             continue;
         }
         let line_indent_lvl = line.chars().take_while(|c| *c == '\t').count();
-        if indent_lvl == line_indent_lvl {
-            let partial_entry: PartialEntry = parse_entry(line)?;
+        if line_indent_lvl < indent_lvl {
+            break;
+        } else if indent_lvl == line_indent_lvl {
+            lines.next(); // consume the line
+            let partial_entry: PartialEntry = parse_entry(&line)?;
             let key = partial_entry.key;
             let val = if let Some(atom_val) = partial_entry.val {
                 atom_val
@@ -50,10 +141,13 @@ pub fn parse_entries(lines: &mut std::str::Lines, indent_lvl: usize) -> Option<V
                 EntryVal::SubEntries(parse_entries(lines, indent_lvl + 1)?)
             };
             entries.push(Entry { key, val });
+        } else {
+            lines.next(); // consume the line
         }
     }
     Some(entries)
 }
+
 
 fn parse_entry(line: &str) -> Option<PartialEntry> {
     let mut parts = line.splitn(2, ':');
