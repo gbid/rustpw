2a9561f63ee716425a8dbbe97a2e3adc85a72364
Gunther Bidlingmaier <g.bidlingmaier@gmail.com>
2023-06-08 21:06:29 +0200

    Add error handling for parsing

--- src/parse.rs
+++ src/parse.rs
@@ -16,32 +16,39 @@ struct PartialEntry {
 }
 
 use std::iter::Peekable;
+use std::str::Lines;
 
-pub fn parse_entries(lines: &mut Peekable<std::str::Lines>, indent_lvl: usize) -> Option<Vec<Entry>> {
-	let mut entries = Vec::new();
-	while let Some(line) = lines.peek().cloned() {
-		if line.is_empty() {
-			lines.next();
-			continue;
-		}
-		let line_indent_lvl = line.chars().take_while(|c| *c == '\t').count();
-		if line_indent_lvl < indent_lvl {
-			break;
-		} else if indent_lvl == line_indent_lvl {
-			lines.next();
-			let partial_entry: PartialEntry = parse_entry(&line)?;
-			let key = partial_entry.key;
-			let val = if let Some(atom_val) = partial_entry.val {
-				atom_val
-			} else {
-				EntryVal::SubEntries(parse_entries(lines, indent_lvl + 1)?)
-			};
-			entries.push(Entry { key, val });
-		} else {
-			lines.next();
-		}
-	}
-	Some(entries)
+#[derive(Debug)]
+pub enum ParseError {
+    InvalidFormat(String),
+}
+
+pub fn parse_entries(lines: &mut Peekable<Lines>, indent_lvl: usize) -> Result<Vec<Entry>, ParseError> {
+    let mut entries = Vec::new();
+    while let Some(line) = lines.peek().cloned() {
+        if line.is_empty() {
+            lines.next();
+            continue;
+        }
+        let line_indent_lvl = line.chars().take_while(|c| *c == '\t').count();
+        if line_indent_lvl < indent_lvl {
+            break;
+        } else if indent_lvl == line_indent_lvl {
+            lines.next();
+            let partial_entry: PartialEntry = parse_entry(&line)
+                .ok_or(ParseError::InvalidFormat(format!("Cannot parse line:{}", line)))?;
+            let key = partial_entry.key;
+            let val = if let Some(atom_val) = partial_entry.val {
+                atom_val
+            } else {
+                EntryVal::SubEntries(parse_entries(lines, indent_lvl + 1)?)
+            };
+            entries.push(Entry { key, val });
+        } else {
+            return Err(ParseError::InvalidFormat(format!("Invalid indentation level on line: {}", line)));
+        }
+    }
+    Ok(entries)
 }
 
 fn parse_entry(line: &str) -> Option<PartialEntry> {
